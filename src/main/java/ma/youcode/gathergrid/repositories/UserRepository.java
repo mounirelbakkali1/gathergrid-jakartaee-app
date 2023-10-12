14bde5f7cf58feaf2c37b4b11ca1c0ae06ab0240
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-10-12 14:48:53 +0100

    add: added userrepository and its implementationÃ

---
+++ src/main/java/ma/youcode/gathergrid/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package ma.youcode.gathergrid.repositories;
+
+
+import ma.youcode.gathergrid.domain.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository {
+    public Optional<User> findByUsername(String username);
+    public void save(User user);
+    public void update(User user);
+    public void delete(User user);
+    public List<User> findAll();
+
+}
---
+++ src/main/java/ma/youcode/gathergrid/repositories/UserRepositoryImpl.java
@@ -0,0 +1,52 @@
+package ma.youcode.gathergrid.repositories;
+
+import jakarta.annotation.Priority;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import ma.youcode.gathergrid.domain.User;
+
+import java.util.List;
+import java.util.Optional;
+
+@ApplicationScoped
+@Priority(1)
+public class UserRepositoryImpl implements UserRepository {
+
+
+    private EntityManager em;
+
+    public UserRepositoryImpl() {
+        EntityManagerFactory emf = jakarta.persistence.Persistence.createEntityManagerFactory("default");
+        em = emf.createEntityManager();
+    }
+
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        System.out.println("Getting user by username: " + username);
+        return em.createQuery("select u from User u where u.username = :username", User.class)
+                .setParameter("username", username)
+                .getResultStream().findAny();
+    }
+
+    @Override
+    public void save(User user) {
+        em.persist(user);
+    }
+
+    @Override
+    public void update(User user) {
+        em.merge(user);
+    }
+
+    @Override
+    public void delete(User user) {
+        em.remove(user);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return em.createQuery("select u from User u", User.class).getResultList();
+    }
+}
