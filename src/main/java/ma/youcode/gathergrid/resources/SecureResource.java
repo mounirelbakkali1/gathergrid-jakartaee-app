de7b539c72af22f0470963df894385e0ce8ee7b1
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-10-16 13:27:28 +0100

    ADD: demo secure servlet config

---
+++ src/main/java/ma/youcode/gathergrid/resources/SecureResource.java
@@ -0,0 +1,45 @@
+package ma.youcode.gathergrid.resources;
+
+import jakarta.security.enterprise.authentication.mechanism.http.CustomFormAuthenticationMechanismDefinition;
+import jakarta.security.enterprise.authentication.mechanism.http.LoginToContinue;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.HttpConstraint;
+import jakarta.servlet.annotation.ServletSecurity;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.security.Principal;
+import java.util.Optional;
+
+@CustomFormAuthenticationMechanismDefinition(
+        loginToContinue = @LoginToContinue(
+                loginPage = "/login",
+                errorPage = "/login?error=failed"
+        )
+)
+@ServletSecurity(@HttpConstraint(rolesAllowed = "admin"))
+@WebServlet("/secure")
+public class SecureResource extends HttpServlet {
+
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                String username = null ;
+                Optional<Principal> userPrincipal = Optional.ofNullable(req.getUserPrincipal());
+                if(userPrincipal.isPresent() && userPrincipal.get().getName() !=null){
+                        username = userPrincipal.get().getName();
+                }
+                resp.getWriter().write(String.format("""
+                        <html>
+                        <body>
+                        
+                        <h3>Hello %s </h3>
+                        
+                        </body>
+                        </html>
+                        
+                        """,username));
+        }
+}
