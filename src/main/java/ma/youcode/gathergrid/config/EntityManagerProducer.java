c16bd9bb1322eb334d29293445a0bc81eb905797
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-10-16 19:33:39 +0100

    FIX : qualifying EntityManager for Injection

--- src/main/java/ma/youcode/gathergrid/config/EntityManagerProducer.java
+++ src/main/java/ma/youcode/gathergrid/config/EntityManagerProducer.java
@@ -1,14 +1,35 @@
 package ma.youcode.gathergrid.config;
 
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Disposes;
+import jakarta.enterprise.inject.Model;
 import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Singleton;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.Persistence;
+import jakarta.persistence.PersistenceContext;
 
+
+@ApplicationScoped
 public class EntityManagerProducer {
 
+
+    private EntityManager em;
+
+    @PersistenceContext(unitName = "primary")
+    public void setEntityManager(EntityManager em) {
+        this.em = em;
+    }
+
     @Produces
-    public EntityManager entityManager(){
-        return Persistence.createEntityManagerFactory("default").createEntityManager();
+    @UserDatabase
+    public EntityManager produceEntityManager() {
+        return em;
+    }
+
+
+
+    public void close(@Disposes @UserDatabase EntityManager em) {
+        em.close();
     }
 }
--- src/main/java/ma/youcode/gathergrid/config/IdentityStoreConfig.java
+++ src/main/java/ma/youcode/gathergrid/config/IdentityStoreConfig.java
@@ -1,26 +1,43 @@
 package ma.youcode.gathergrid.config;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.security.enterprise.credential.Credential;
 import jakarta.security.enterprise.credential.UsernamePasswordCredential;
 import jakarta.security.enterprise.identitystore.CredentialValidationResult;
 import jakarta.security.enterprise.identitystore.IdentityStore;
+import ma.youcode.gathergrid.domain.User;
+import ma.youcode.gathergrid.repositories.UserRepository;
+import ma.youcode.gathergrid.repositories.UserRepositoryImpl;
+import ma.youcode.gathergrid.utils.PasswordHashUtil;
 
 import java.util.HashSet;
+import java.util.Optional;
 
 import static java.util.Arrays.asList;
 
 @ApplicationScoped
 public class IdentityStoreConfig implements IdentityStore {
+    @Inject
+    private PasswordHashUtil pwdUtil;
+    @Inject
+    private UserRepository userRepository;
 
     @Override
     public CredentialValidationResult validate(Credential credential) {
-        System.out.println("validating --------------------");
         UsernamePasswordCredential passwordCredential = (UsernamePasswordCredential) credential;
-        System.out.println(passwordCredential.compareTo("admin","admin"));
-        if(passwordCredential.compareTo("admin","admin")){
-            return new CredentialValidationResult("admin",new HashSet<>(asList("admin","agent")));
+        Optional<User> optionalUser = userRepository.findByUsername(passwordCredential.getCaller());
+        if(optionalUser.isEmpty()) return CredentialValidationResult.INVALID_RESULT;
+        else {
+            User user = optionalUser.get();
+            System.out.println("user: " + user);
+            System.out.println("credential: " + passwordCredential.getCaller() + " " + passwordCredential.getPasswordAsString());
+            if (passwordCredential.compareTo(user.getUsername(), user.getPassword())) {
+                return new CredentialValidationResult(user.getName(),new HashSet<>(asList(user.getRole().getName())));
+            }
         }
         return CredentialValidationResult.INVALID_RESULT;
     }
+
+
 }
