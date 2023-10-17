9a2e3cb74d936eb05e7bc3f3a8abfdf26ad2203b
MohamedJamh <jam7.mohamed@gmail.com>
2023-10-17 16:52:31 +0100

    eventFeature : Fix - fixing Generic type of create Event method in event service

---
+++ src/main/java/ma/youcode/gathergrid/service/EventService.java
@@ -0,0 +1,31 @@
+package ma.youcode.gathergrid.service;
+
+import jakarta.inject.Inject;
+import ma.youcode.gathergrid.domain.Event;
+import ma.youcode.gathergrid.repositories.EventRepository;
+import ma.youcode.gathergrid.utils.Response;
+
+import java.util.List;
+
+public class EventService {
+    private EventRepository eventRepository;
+
+    @Inject
+    public EventService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+    public EventService() {
+    }
+
+    public Response<Event> createEvent(Event event){
+        //if(event)
+        // new Response ;
+
+        //Response<Event> eventResponse = new Response<>();
+        //eventResponse.setError(List.of());
+        //return eventResponse;
+
+        //eventRepository.save(event);
+        //return event;
+    }
+}
