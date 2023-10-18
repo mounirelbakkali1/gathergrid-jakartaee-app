a44ee7bab6701593853945db8ca8a73f84d25d76
MohamedJamh <jam7.mohamed@gmail.com>
2023-10-18 09:52:54 +0100

    eventFeature : Add - adding dashboard Servlet to handle dashboard data workflow

--- src/main/java/ma/youcode/gathergrid/resources/DashboardServlet.java
+++ src/main/java/ma/youcode/gathergrid/resources/DashboardServlet.java
@@ -1,25 +1,32 @@
 package ma.youcode.gathergrid.resources;
 
+import jakarta.inject.Inject;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import ma.youcode.gathergrid.repositories.EventRepositoryImpl;
 import ma.youcode.gathergrid.service.EventService;
 
 import java.io.IOException;
 
 @WebServlet(name = "DashboardServlet", value = "/dashboard")
-public class EventServlet extends HttpServlet {
-
-    private EventService eventService = new EventService();
+public class DashboardServlet extends HttpServlet {
+    @Inject
+    private EventService eventService;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        //req.setAttribute("events", EventService.);
-
-        // Forward the request to the JSP page
-        //RequestDispatcher dispatcher = request.getRequestDispatcher("/yourpage.jsp").forward(request, response);
+        resp.getWriter().write(String.format("""
+                        <html>
+                        <body>
+                        
+                        <h3>Hello %s </h3>
+                        
+                        </body>
+                        </html>
+                        
+                        ""","me"));
+        //req.setAttribute("events",eventService.getAllEvents());
+        //req.getRequestDispatcher("/WEB-INF/dashboard.jsp").forward(req, resp);
     }
 }
