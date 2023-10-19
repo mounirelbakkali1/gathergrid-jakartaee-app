be801852b4d8354d60fbf81f976c911258e53559
MohamedJamh <jam7.mohamed@gmail.com>
2023-10-19 15:46:43 +0100

    eventFeature : Fix - fixing returne type of method get all events towards event jsp page

--- src/main/java/ma/youcode/gathergrid/resources/EventServlet.java
+++ src/main/java/ma/youcode/gathergrid/resources/EventServlet.java
@@ -13,55 +13,73 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import ma.youcode.gathergrid.domain.Category;
 import ma.youcode.gathergrid.domain.Event;
+import ma.youcode.gathergrid.domain.Organization;
 import ma.youcode.gathergrid.service.CategoryService;
 import ma.youcode.gathergrid.service.EventService;
+import ma.youcode.gathergrid.service.UserService;
 import ma.youcode.gathergrid.utils.Response;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 
 @ServletSecurity(value = @HttpConstraint(rolesAllowed = {"ADMIN","USER"}))
 @WebServlet(name = "EventServlet",value = "/event")
 public class EventServlet extends HttpServlet {
+    @Inject
+    private UserService userService;
     @Inject
     private EventService eventService;
     @Inject
     private CategoryService categoryService;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String userName = req.getUserPrincipal().getName();
+        //User userId = userService.findUserByUsername(userName).get().getId();
+
         req.setAttribute("action",req.getParameter("action"));
         req.setAttribute("categories",categoryService
                                             .getAllCategories()
                                             .getResult());
+
         req.getRequestDispatcher("/WEB-INF/event.jsp").forward(req,resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Response<Event> eventResponse = new Response<>();
+        Date eventDate = new Date(1970);
         try {
-            Date eventDate = new Date(
+            eventDate = new Date(
                     new SimpleDateFormat("dd-MM-yyyy")
                     .parse(req.getParameter("date")
             ).getTime());
-
-            long millisecondsSinceMidnight = 12 * 60 * 60 * 1000; // 12:00:00 in milliseconds
-            java.sql.Time time = new java.sql.Time(millisecondsSinceMidnight);
-
-            eventResponse = eventService.createEvent(
-                    new Event(
-                            //null,
-                            //req.getParameter("name"),
-                            //eventDate,
-                            //time,
-                            //req.getParameter("location"),
-                            //req.getParameter("description")
-                    )
-            );
         } catch (Exception e) {
-             eventResponse.setError(List.of(new Error("Date error")));
+            eventResponse.setError(List.of(new Error("Date error")));
         }
-        req.setAttribute("result",eventResponse);
+
+        long millisecondsSinceMidnight = 12 * 60 * 60 * 1000; // 12:00:00 in milliseconds
+        java.sql.Time time = new java.sql.Time(millisecondsSinceMidnight);
+
+        Category category = categoryService.getCategoryById(
+                Long.parseLong(req.getParameter("category"))
+        ).getResult();
+
+        eventResponse = eventService.createEvent(
+                Event.builder()
+                        .name(req.getParameter("name"))
+                        .description(req.getParameter("description"))
+                        .date(eventDate)
+                        .hour(time)
+                        .location(req.getParameter("location"))
+                        .category(category)
+                        .organization(
+                                Organization.builder().id(Long.parseLong("1")).build()
+                        )
+                        .build()
+        );
+        req.setAttribute("response",eventResponse);
         req.getRequestDispatcher("/WEB-INF/event.jsp").forward(req,resp);
     }
 }
